9e110cb76eed43011666aec39dbbb341807c9dec
Alex Place <alex.place.7@gmail.com>
2016-07-24 20:23:13 -0400

    added attribute for errors

--- dataisbeautiful/src/main/java/co/porkopolis/utils/AttributeConstants.java
+++ dataisbeautiful/src/main/java/co/porkopolis/utils/AttributeConstants.java
@@ -4,5 +4,6 @@ public class AttributeConstants {
 	
 	public static final String SUMMONER_NAME = "summonerName";
 	public static final String BASIC_SUMMONER = "basicSummoner";
+	public static final String ERROR = "actionerror";
 
 }
