a83974f654729efda4400f601be35d398807b491
Alex Place <alex.place.7@gmail.com>
2016-08-02 23:46:45 -0400

    oops

--- dataisbeautiful/src/main/java/co/porkopolis/dao/BasicSummonerDAO.java
+++ dataisbeautiful/src/main/java/co/porkopolis/dao/BasicSummonerDAO.java
@@ -1,14 +1,12 @@
 package co.porkopolis.dao;
 
-import org.springframework.dao.EmptyResultDataAccessException;
-
 import net.rithms.riot.api.endpoints.summoner.dto.Summoner;
 
 public interface BasicSummonerDAO {
 
 	public void insert(Summoner summoner);
 
-	public Summoner findByName(String name) throws EmptyResultDataAccessException;
+	public Summoner findByName(String name);
 
 	public Summoner findById(int id);
 
